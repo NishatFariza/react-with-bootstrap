99f00e709f15ff721eb19c430371065958e1cbdf
NishatFariza <nishatfarzia78@gmail.com>
2022-03-30 15:13:13 -0700

    bootstrap add

--- src/components/CardGroup/CardGroup.jsx
+++ src/components/CardGroup/CardGroup.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const CardGroup = () => {
+
+    const products =[
+        {id:1, name:'Laptop pro', price:125000},
+        {id:2, name:'Laptop mini', price:80000},
+        {id:3, name:'Laptop pro x', price:180000}
+
+    ]
     return (
         <div>
             <h2>This is my cards</h2>
+            <div className="card-group">
+                {
+                    products.map(product =>)
+                }
+            </div>
         </div>
     );
 };
